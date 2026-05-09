dee3f05e6e5407c9e751dff3b7f4f19886124f98
LearnWithRehan <learnwithrehan86147@gmail.com>
2026-05-09 16:28:53 +0530

    Add Quotient And Remainder

---
+++ QuotientRemainder.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class QuotientRemainder {
+    public static void main(String[] args) {
+        //from user input
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Dividend:-");
+        int dividend = sc.nextInt();
+        System.out.println("Enter Divisor:-");
+        int divisor= sc.nextInt();
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+        System.out.println("Quotient:- " + quotient) ;
+        System.out.println("Remainder:- " + remainder);
+    }
+}
