1ae7858b757a987daf9c35c580f0014a34edb1b7
LearnWithRehan <learnwithrehan86147@gmail.com>
2026-05-29 17:49:25 +0530

    add Program38

--- Program38.java
+++ Program38.java
@@ -1,3 +1,23 @@
-public class Program38 {
+import java.util.Scanner;
 
+public class Program38 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a decimal number");
+        int decimal = sc.nextInt();
+        int octal = ConvertDeciToOcta(decimal);
+        System.out.printf("%d in decimal = %d in octal",
+                decimal,octal);
+        sc.close();
+
+    }
+    public static int ConvertDeciToOcta(int decimal){
+        int octalNum = 0, i = 1;
+        while (decimal != 0){
+            octalNum += (decimal % 8) * i;
+            decimal /= 8;
+            i *= 10;
+        }
+        return octalNum;
+    }
 }
